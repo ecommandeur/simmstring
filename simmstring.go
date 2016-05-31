bdd47a9535e08c6ee245ab890047ca0672117360
ecommand <ecommandeur@nl.dimins.com>
2016-05-31 22:48:35 +0200

    added acknowledgements, ran gofmt over src

--- simmstring.go
+++ simmstring.go
@@ -1,9 +1,16 @@
 package main
 
-import (
-  "fmt"
-)
+import "fmt"
 
 func main() {
-    fmt.Println("Simmer down...")
+	fmt.Println("Simmer down...")
+
+	// Parse arguments here
+
+	// Read source strings from a file
+	//	f, err := os.Open("resources/test_strings.txt")
+	//	if err != nil {
+	//		fmt.Println("Oops")
+	//		os.Exit(1)
+	//	}
 }
