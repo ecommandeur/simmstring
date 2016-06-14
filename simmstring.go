59b7ce3bd32788d259712ce05c75f0966289e291
ecommand <ecommandeur@nl.dimins.com>
2016-06-15 00:35:33 +0200

    get args from commandline and towards returning multiple matches

--- simmstring.go
+++ simmstring.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 
@@ -9,19 +10,38 @@ import (
 )
 
 // TODO Refactor big hairy main - Apply boy scout rule
+// go run simmstring.go -source resources\test_strings.txt -target resources\ref_strings.txt
 func main() {
-	fmt.Println("Simmer down...")
+	//
+	// Jaro Winkler parameters
+	//
 	boostThreshold := 0.7 // boostThreshold = minimum score for a string that gets boosted. This value was set to 0.7 in Winkler's papers.
 	prefixSize := 4       // prefixSize = size of the initial prefix considered. This value was set to 4 in Winkler's papers.
-	verbose := true
 
-	// TODO Parse arguments here
+	//
+	// ARGUMENTS
+	//
+	var src string
+	var tar string
+	var verbose bool
+	//nummatches := 3
+
+	flag.StringVar(&src, "source", "", "(Required) Path to file with source strings.")
+	flag.StringVar(&tar, "target", "", "(Required) Path to file with target strings.")
+	flag.BoolVar(&verbose, "v", false, "(Optional) Verbose mode.")
+
+	flag.Parse()
+
+	if src == "" || tar == "" {
+		printUsage()
+		os.Exit(1)
+	}
 
 	//Read source strings from a file
-	source, err := os.Open("resources/test_strings.txt")
+	source, err := os.Open(src)
 	exitOnError(err, "Oops cannot find source")
 
-	target, err := os.Open("resources/ref_strings.txt")
+	target, err := os.Open(tar)
 	exitOnError(err, "Oops cannot find target")
 
 	defer target.Close()
@@ -74,17 +94,61 @@ func main() {
 		// TODO do sth with error
 	}
 
+	/* test sorting of SimPair
+	var sp []*SimPair
+	sp = append(sp, &SimPair{Source: "a", Target: "b", Distance: 2.5})
+	sp = append(sp, &SimPair{Source: "a", Target: "c", Distance: 1.5})
+	fmt.Println(sp)
+	sort.Sort(ByDistance(sp))
+	fmt.Println(sp)
+	*/
+
 	//At this point we have the best matches
 	// TODO write out to a file
+	// or just rely on piping to output ... ?
+	// what is more efficient ?
 	for i := range bestMatches {
 		fmt.Print(bestMatches[i][0], ",", bestMatches[i][1], "\n")
 	}
 
 }
 
+// SimPair is pair of strings, a Source and a Target string, along with the Distance between that pair of strings
+type SimPair struct {
+	Source   string
+	Target   string
+	Distance float64
+}
+
+func (s SimPair) String() string { return fmt.Sprintf("%s %s - %v", s.Source, s.Target, s.Distance) }
+
+// ByDistance is for sorting SimPairs by distance
+type ByDistance []*SimPair
+
+// Len method of sort interface for ByDistance
+func (d ByDistance) Len() int { return len(d) }
+
+// Swap method of sort interface for ByDistance
+func (d ByDistance) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
+
+// Less method of sort interface for ByDistance
+func (d ByDistance) Less(i, j int) bool { return d[i].Distance < d[j].Distance }
+
 func exitOnError(e error, msg string) {
 	if e != nil {
-		fmt.Println(msg)
+		fmt.Print(msg, "\n\n")
+		printUsage()
 		os.Exit(1)
 	}
+}
+
+// printUsage prints help output
+func printUsage() {
+	println("simmstring version 0.1-SNAPSHOT")
+	println("")
+	println("Usage:")
+	flag.PrintDefaults()
+	println("")
+	println("Examples:")
+	println("  simmstring -source resoures\\test_strings.txt -target resources\\ref_strings.txt")
 }
